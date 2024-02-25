f2ea2f3cf9a5f8106f80d533fa61f8263e6144fc
KingNelx <tapiajonel606@gmail.com>
2024-02-26 01:20:15 +0800

    created springboot environment

---
+++ lost-n-found-be/src/main/java/lostnfoundbe/LostNFoundBeApplication.java
@@ -0,0 +1,13 @@
+package lostnfoundbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostNFoundBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostNFoundBeApplication.class, args);
+	}
+
+}
---
+++ lost-n-found-be/src/test/java/lostnfoundbe/LostNFoundBeApplicationTests.java
@@ -0,0 +1,13 @@
+package lostnfoundbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostNFoundBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
